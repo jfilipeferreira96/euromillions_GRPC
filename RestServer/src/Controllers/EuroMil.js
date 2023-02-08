bceb38ece4be0da488f3d1185f5c2d6bf007cd3f
Filipe Ferreira <filipeferreira96@hotmail.com>
2023-02-08 19:35:50 +0000

    finishing touches

--- RestServer/src/Controllers/EuroMil.js
+++ RestServer/src/Controllers/EuroMil.js
@@ -1,25 +1,23 @@
-const caller = require('grpc-caller')
+const caller = require("grpc-caller");
 const PROTO_PATH = "./src/proto/registerEuroMil.proto";
-const client = caller('localhost:50051', PROTO_PATH, "Euromil");
+const client = caller("localhost:50051", PROTO_PATH, "Euromil");
 
 class Controller {
-  
-  async grpcRequest(req, res){
+  async grpcRequest(req, res) {
     const { key, checkid } = req.body;
     let data = { message: `Something went wrong.` };
     let responseCode = 400;
 
-    try{
+    try {
       console.log(`Sent to gRPC server the following object:`, JSON.stringify({ key, checkid }));
-      const response = await client.RegisterEuroMil({ key: "123", checkid: "################" })
-      if (response)
-      {
+      const response = await client.RegisterEuroMil({ key, checkid });
+      if (response) {
         console.log(`Server returned the following object:`, JSON.stringify(response));
         data = response;
         responseCode = 200;
-      } 
+      }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
 
     res.status(responseCode).json(data);
